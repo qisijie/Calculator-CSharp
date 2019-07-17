594c43a306a872abce223d30637eb0b894ae65a0
an_dark <qc_cc585f44470549f19c4721fa01465733@git.code.tencent.com>
2019-07-17 17:11:46 +0800

    2.0

---
+++ Calculator_WinForm/Main.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_WinForm
+{
+    public partial class Main : Form
+    {
+        double number1 = 0, number2 = 0, result;
+        int inputnumber, pointnumber;
+
+        enum Operator { none, addition, minus, multiplication, division, power, squareroot }
+        Operator mode = Operator.none;
+        bool isequal = false, ispoint = false, isnegative = false;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            inputnumber = 1;
+            addnumber(inputnumber);
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            inputnumber = 2;
+            addnumber(inputnumber);
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            inputnumber = 3;
+            addnumber(inputnumber);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            inputnumber = 4;
+            addnumber(inputnumber);
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            inputnumber = 5;
+            addnumber(inputnumber);
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            inputnumber = 6;
+            addnumber(inputnumber);
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            inputnumber = 7;
+            addnumber(inputnumber);
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            inputnumber = 8;
+            addnumber(inputnumber);
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            inputnumber = 9;
+            addnumber(inputnumber);
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            inputnumber = 0;
+            addnumber(inputnumber);
+        }
+
+        private void btnpoint_Click(object sender, EventArgs e)
+        {
+            if (ispoint == false)
+            {
+                pointnumber = 1;
+                if (mode == Operator.none || mode == Operator.squareroot)
+                {
+                    labelout.Text = Convert.ToString(number1) + ".";
+                }
+                else
+                {
+                    labelout.Text = Convert.ToString(number2) + ".";
+                }
+                ispoint = true;
+            }
+        }
+
+        private void btnequal_Click(object sender, EventArgs e)
+        {
+            switch (mode)
+            {
+                case Operator.addition:
+                    result = number1 + number2;
+                    break;
+                case Operator.minus:
+                    result = number1 - number2;
+                    break;
+                case Operator.multiplication:
+                    result = number1 * number2;
+                    break;
+                case Operator.division:
+                    result = number1 / number2;
+                    break;
+                case Operator.power:
+                    result = Math.Pow(number1, number2);
+                    break;
+                case Operator.squareroot:
+                    result = Math.Sqrt(number1);
+                    break;
+            }
+            number1 = 0;
+            number2 = 0;
+            isequal = true;
+            labelbefore.Text = "";
+            labelmode.Text = "";
+            labelout.Text = Convert.ToString(result);
+        }
+
+        private void btnaddition_Click(object sender, EventArgs e)
+        {
+            mode = Operator.addition;
+            switchmode();
+        }
+
+        private void btnsubtraction_Click(object sender, EventArgs e)
+        {
+            mode = Operator.minus;
+            switchmode();
+        }
+
+        private void btnmultiplication_Click(object sender, EventArgs e)
+        {
+            mode = Operator.multiplication;
+            switchmode();
+        }
+
+        private void btndivision_Click(object sender, EventArgs e)
+        {
+            mode = Operator.division;
+            switchmode();
+        }
+
+        private void btnpower_Click(object sender, EventArgs e)
+        {
+            mode = Operator.power;
+            switchmode();
+        }
+
+        private void btnsquareroot_Click(object sender, EventArgs e)
+        {
+            mode = Operator.squareroot;
+            switchmode();
+        }
+
+        private void btnnegative_Click(object sender, EventArgs e)
+        {
+            if (isnegative == false)
+            {
+                isnegative = true;
+            }
+            else
+            {
+                isnegative = false;
+            }
+
+            if (mode == Operator.none)
+            {
+                number1 = number1 * -1;
+                labelout.Text = Convert.ToString(number1);
+            }
+            else
+            {
+                number2 = number2 * -1;
+                labelout.Text = Convert.ToString(number2);
+            }
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            labelout.Text = Convert.ToString(number1);
+            labelmode.Text = "";
+            labelbefore.Text = "";
+        }
+
+        private void btnclean_Click(object sender, EventArgs e)
+        {
+            cleanall();
+        }
+
+        private void btnback_Click(object sender, EventArgs e)
+        {
+            if (ispoint == false)
+            {
+                if (mode == Operator.none || mode == Operator.squareroot)
+                {
+                    if (number1 >= 10 || number1 <= -10)
+                    {
+                        number1 = Convert.ToDouble(Convert.ToString(number1).Substring(0, Convert.ToString(number1).Length - 1));
+                    }
+                    else if (number1 < 10 || number1 > -10)
+                    {
+                        number1 = 0;
+                    }
+
+                    labelout.Text = Convert.ToString(number1);
+                }
+                else
+                {
+                    if (number2 >= 10 || number2 <= -10)
+                    {
+                        number2 = Convert.ToDouble(Convert.ToString(number2).Substring(0, Convert.ToString(number2).Length - 1));
+                    }
+                    else if (number2 < 10 || number2 > -10)
+                    {
+                        number2 = 0;
+                    }
+
+                    labelout.Text = Convert.ToString(number2);
+                }
+            }
+            else
+            {
+                if (mode == Operator.none || mode == Operator.squareroot)
+                {
+
+                    number1 = Convert.ToDouble(Convert.ToString(number1).Substring(0, Convert.ToString(number1).Length - 1));
+                    labelout.Text = Convert.ToString(number1);
+                    pointnumber = pointnumber - 1;
+                    if (pointnumber == 1)
+                    {
+                        ispoint = false;
+                        pointnumber = 0;
+                    }
+                }
+                else
+                {
+                    number2 = Convert.ToDouble(Convert.ToString(number2).Substring(0, Convert.ToString(number2).Length - 1));
+                    labelout.Text = Convert.ToString(number2);
+                    pointnumber = pointnumber - 1;
+                    if (pointnumber == 1)
+                    {
+                        ispoint = false;
+                        pointnumber = 0;
+                    }
+                }
+            }
+        }
+
+
+
+
+
+        public void addnumber(int an)
+        {
+            if (isnegative == true)
+            {
+                if (pointnumber == 0)
+                {
+                    if (mode == Operator.none || mode == Operator.squareroot)
+                    {
+                        number1 = number1 * 10 - an;
+                        labelout.Text = Convert.ToString(number1);
+                    }
+                    else
+                    {
+                        number2 = number2 * 10 - an;
+                        labelout.Text = Convert.ToString(number2);
+                    }
+                }
+                else
+                {
+                    pointnumber = pointnumber + 1;
+                    if (mode == Operator.none || mode == Operator.squareroot)
+                    {
+                        number1 = number1 - an / Math.Pow(10, pointnumber - 1);
+                        labelout.Text = Convert.ToString(number1);
+                    }
+                    else
+                    {
+                        number2 = number2 - an / Math.Pow(10, pointnumber - 1);
+                        labelout.Text = Convert.ToString(number2);
+                    }
+                }
+            }
+            else
+            {
+                if (pointnumber == 0)
+                {
+                    if (mode == Operator.none || mode == Operator.squareroot)
+                    {
+                        number1 = number1 * 10 + an;
+                        labelout.Text = Convert.ToString(number1);
+                    }
+                    else
+                    {
+                        number2 = number2 * 10 + an;
+                        labelout.Text = Convert.ToString(number2);
+                    }
+                }
+                else
+                {
+                    pointnumber = pointnumber + 1;
+                    if (mode == Operator.none || mode == Operator.squareroot)
+                    {
+                        number1 = number1 + an / Math.Pow(10, pointnumber - 1);
+                        labelout.Text = Convert.ToString(number1);
+                    }
+                    else
+                    {
+                        number2 = number2 + an / Math.Pow(10, pointnumber - 1);
+                        labelout.Text = Convert.ToString(number2);
+                    }
+                }
+            }
+        }
+
+        public void switchmode()
+        {
+            switch (mode)
+            {
+                case Operator.addition:
+                    labelmode.Text = "+";
+                    break;
+                case Operator.minus:
+                    labelmode.Text = "-";
+                    break;
+                case Operator.multiplication:
+                    labelmode.Text = "*";
+                    break;
+                case Operator.division:
+                    labelmode.Text = "/";
+                    break;
+                case Operator.power:
+                    labelmode.Text = "^";
+                    break;
+                case Operator.squareroot:
+                    labelmode.Text = "√";
+                    break;
+            }
+
+            ispoint = false;
+            pointnumber = 0;
+            if (isequal == true)
+            {
+                number1 = result;
+            }
+            if (mode != Operator.squareroot)
+            {
+                labelbefore.Text = Convert.ToString(number1);
+                labelout.Text = Convert.ToString(number2);
+            }
+        }
+
+        public void cleanall()
+        {
+            number1 = 0;
+            number2 = 0;
+            labelout.Text = Convert.ToString(number1);
+            labelbefore.Text = "";
+            labelmode.Text = "";
+            isequal = false;
+            mode = Operator.none;
+            ispoint = false;
+            pointnumber = 0;
+            isnegative = false;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,41 +1,6 @@
 # CalOfan_dark
 My calculator,and I wrote it in C# by Microsoft Visual Studio.You can change it, but you must indicate the source (an_dark).
 
-# Update log(English version)
-
-|version|update content|
-|:-|:-:|
-|snow leopard(v1.2)|First upload.|
-|siberian tiger(v1.3)|1.Add some new volume calculations. 2.Modify the wrong content.|
-|dragon li(v1.4)|1.Add more new volume calculations. 2.Modify the wrong content.|
-
-# 更新日志(简体中文 版本)
-
-|版本|更新内容|
-|:-|:-:|
-|雪豹(v1.2)|首次上传.|
-|西伯利亚虎(v1.3)|1.添加一些新的体积计算. 2.修改错误的内容.|
-|狸花猫(v1.4)|1.添加更多新的体积计算. 2.修改错误的内容.|
-
-# 更新日誌(繁體中文 版本)
-
-|版本|更新內容|
-|:-|:-:|
-|雪豹(v1.2)|首次上傳.|
-|西伯利亞虎(v1.3)|1.添加一些新的體積計算. 2.修改錯誤的內容.|
-|狸花貓(v1.4)|1.添加更多新的體積計算. 2.修改錯誤的內容.|
-
-*****
-Where can you find me?
-你能在哪找到我?
-你能在哪找到我?
-↓ Here 这里 這裡
-
-[![github](img/GitHub.png)](https://github.com/qisijie) [![gitee](img/Gith.png)](https://gitee.com/qisijie) [![bilibili](img/bilibili%20-哔哩哔哩_B.png)](https://space.bilibili.com/189781174) [![neteasycloudmusic](img/NetEase%20-%E7%BD%91%E6%98%93_music%20-%E9%9F%B3%E4%B9%90.png)](https://music.163.com/#/user/home?id=616566046)[![xiaomi](img/xiaomi%20-%E5%B0%8F%E7%B1%B3.png)](https://order.mi.com/portal?r=38760.1557405716)[![youku](img/youku%20-%E4%BC%98%E9%85%B7_B.png)](http://i.youku.com/qinsijie)[![toudou](img/tudou%20-%E5%9C%9F%E8%B1%86.png)](http://id.tudou.com/qinsijie)
-
-*****
-My mail
-我的邮箱
-我的郵箱
-
-qq[![qqmail](img/mail.png)](mailto:1059022187@qq.com)
+# Wanana learn more, see branch(master)'s README.md
+# 想要了解跟多, 见分支(master)的README.md
+# 想要了解跟多, 見分支(master)的README.md
